590154dad77a58efd51945b3c372e3288258fd5d
SegaraRai <29276700+SegaraRai@users.noreply.github.com>
2026-03-28 22:22:34 +0900

    feat: support wasm build

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -4,11 +4,27 @@ fn main() {
     let tsx_dir = root_dir.join("tsx").join("src");
     let common_dir = root_dir.join("common");
 
-    let mut config = cc::Build::new();
-    config.include(&typescript_dir);
-    config
+    let mut c_config = cc::Build::new();
+    c_config.include(&typescript_dir);
+    c_config.include(&tsx_dir);
+    c_config
         .flag_if_supported("-std=c11")
         .flag_if_supported("-Wno-unused-parameter");
+
+    let target = std::env::var("TARGET").unwrap();
+    if target.contains("windows-msvc") {
+        c_config.flag("/utf-8");
+    }
+    if target == "wasm32-unknown-unknown" {
+        let Ok(wasm_headers) = std::env::var("DEP_TREE_SITTER_LANGUAGE_WASM_HEADERS") else {
+            panic!("Environment variable DEP_TREE_SITTER_LANGUAGE_WASM_HEADERS must be set");
+        };
+        c_config.include(&wasm_headers);
+        c_config.define("NEED_WASM_EXTRA_H", None);
+
+        // Prevent duplicate symbol error in WASM linking.
+        c_config.define("NDEBUG", None);
+    }
 
     for path in &[
         typescript_dir.join("parser.c"),
@@ -16,7 +32,7 @@ fn main() {
         tsx_dir.join("parser.c"),
         tsx_dir.join("scanner.c"),
     ] {
-        config.file(path);
+        c_config.file(path);
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
     }
 
@@ -25,5 +41,5 @@ fn main() {
         common_dir.join("scanner.h").to_str().unwrap()
     );
 
-    config.compile("tree-sitter-typescript");
+    c_config.compile("tree-sitter-typescript");
 }
